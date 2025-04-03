4fdfffc9a8ea411ce17061e1a25d24330c486dc2
ipn6 <ipn6@alu.ua.es>
2025-04-03 16:05:20 +0200

    b

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://easymusic-dghga8hafehtbqgc.spaincentral-01.azurewebsites.net'  // 🔹 Backend en Azure
+    apiUrl: 'https://easymusic-dghga8hafehtbqgc.spaincentral-01.azurewebsites.net/'  // 🔹 Backend en Azure
   };
