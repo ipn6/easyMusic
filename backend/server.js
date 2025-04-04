b60330e90db10fdc604abe095c42dc167b39a81d
ipn6 <ipn6@alu.ua.es>
2025-04-04 18:52:49 +0200

    a

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
-const sharp = require('sharp');
+//const sharp = require('sharp');
 
 const storage = multer.memoryStorage();
 const upload = multer({
